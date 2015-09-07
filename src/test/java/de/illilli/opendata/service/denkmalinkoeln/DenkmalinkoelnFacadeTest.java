b5e530bb222a4be349599b19798aa2ba438866da
Wolfram <eberius@gmail.com>
2015-09-07 06:21:36 +0200

    set ignore

--- src/test/java/de/illilli/opendata/service/denkmalinkoeln/DenkmalinkoelnFacadeTest.java
+++ src/test/java/de/illilli/opendata/service/denkmalinkoeln/DenkmalinkoelnFacadeTest.java
@@ -3,12 +3,14 @@ package de.illilli.opendata.service.denkmalinkoeln;
 import java.io.IOException;
 
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 /**
  * Test the write method.
  *
  */
+@Ignore
 public class DenkmalinkoelnFacadeTest {
 
 	@Before
